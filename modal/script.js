c07155f955af1a67d45452a55054655a192d4828
Chris Taylor <134958907+cmtayl0r@users.noreply.github.com>
2023-08-18 15:17:51 +0200

    add keyboard events for modal

--- modal/script.js
+++ modal/script.js
@@ -5,7 +5,7 @@ const overlay = document.querySelector('.overlay');
 const btnShowModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 
-// Create reusable closeModal function to remove hidden classes
+// Create reusable openModal function to remove hidden classes
 const openModal = function () {
     // remove hidden classes
     modal.classList.remove('hidden');
@@ -26,3 +26,17 @@ for (let i = 0; i < btnShowModal.length; i++) {
 // we want it to only execute when this DOM event occurs, so no ()
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// Global event listener for keyboard events
+// We are listening for key events everywhere
+// Pass in "e" object as an argument
+// e can then allow us to specify key value in the object
+document.addEventListener('keydown', function (e) {
+    console.log(e.key);
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        // if Escape key pressed
+        // and IF modal does not contain the hidden class, then execute function
+        // We only want keyboard event to work when its visible
+        closeModal();
+    }
+});
